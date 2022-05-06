79dd1a90e7c4481dbe8cb40ef3426325631c676f
RonaldGarciaValdivia <rgarciava@unsa.edu.pe>
2022-05-06 00:36:18 -0500

    Se aplico Math para hallar una hipotenusa

---
+++ ejemplo_Math.java
@@ -0,0 +1,8 @@
+public class ejemplo_Math{
+    public static void main(String[] args){
+        int numero1 = 3;
+        int numero2 = 4;
+        double numero3 = Math.sqrt((Math.pow(numero1, 2)+(Math.pow(numero2, 2))));
+        System.out.println("Hipotenusa: " + numero3);
+    }
+}
