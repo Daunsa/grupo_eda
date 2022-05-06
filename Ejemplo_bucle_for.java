b4824e470ce9616c1a44e15004ffaf7502a1b11d
Daniel Tapia <dtapias@unsa.edu.pe>
2022-05-05 20:03:31 -0500

    Se creo el ejemplo de uso de bucle for

---
+++ Ejemplo_bucle_for.java
@@ -0,0 +1,8 @@
+public class Ejemplo_bucle_for {
+    public static void main(String[] args) {
+    System.out.println("Ejemplo de uso de bucle for:");
+        for (int i = 0; i <= 10; i += 1) {
+            System.out.println("Iteracion numero " + i);
+        }
+    }
+}
