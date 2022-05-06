747a88a30947acc45efef42ca036aefecdf30fac
benjaminsuclle <msuclles@unsa.edu.pe>
2022-05-06 08:27:21 -0500

    Ejemplo comments terminado

---
+++ Ejemplo_Comments.java
@@ -0,0 +1,14 @@
+public class Ejemplo_Comments {
+	public static void main(String[] args){
+		//este es un comentario normal
+		/*
+		 esta es un comentario multilinea
+		 asi masomenos
+		 es muy practico
+		 * */
+		System.out.println("Solo hay comentarios");
+
+	}
+	
+}
+
