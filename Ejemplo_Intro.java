bee35a3e4f7840e4f93fba54017b11e9d24aa068
benjaminsuclle <msuclles@unsa.edu.pe>
2022-05-06 07:57:05 -0500

    Ejemplo de Intro completo(Resumen)

---
+++ Ejemplo_Intro.java
@@ -0,0 +1,11 @@
+public class Ejemplo_Intro{
+	public static void main(String[] args){
+		System.out.println("Que es java?\nJava es un entorno muy poderoso y veloz, lleno de una gama muy alta de posibilidades para desarrollar");
+		System.out.println("-Apps");
+		System.out.println("-Juegos");
+		System.out.println("-Paginas Web");
+		System.out.println("-Servidores");
+		System.out.println("\nAdemas por ser muy cercano a C++ y C# es muy practico y adaptable");
+	}
+}
+
