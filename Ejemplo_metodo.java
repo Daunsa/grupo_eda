be03677193487352589396571f941f075a7a99fb
Daniel Tapia <dtapias@unsa.edu.pe>
2022-05-05 21:21:49 -0500

    Se añadio el ejemplo de uso de metodos

---
+++ Ejemplo_metodo.java
@@ -0,0 +1,11 @@
+public class Ejemplo_metodo {
+    public static void main(String[] args) {
+        metodo();
+        metodo();
+        metodo();
+    }
+    
+    static void metodo() {
+        System.out.println("Hola desde el metodo estatico");
+    }
+}
