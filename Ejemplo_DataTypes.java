82b40720f6573e66f3a2fcf03e97c60aa4f30145
benjaminsuclle <msuclles@unsa.edu.pe>
2022-05-06 09:35:30 -0500

    Ejemplo de tipos de datos terminado y culmina mi participacion

---
+++ Ejemplo_DataTypes.java
@@ -0,0 +1,26 @@
+public class Ejemplo_DataTypes {
+	public static void main(String[] args){
+		int entero= 5;
+		float flotante = 5.99f;
+		char caracter = 'D';
+		boolean booleano = true;
+		String cadena = "Hello";
+
+		//numeros
+		byte unbyte = 100;
+		short unshort = 5000;
+		int unentero = 100000;
+		long unlong = 15000000000L;
+		float unfloat = 5.75f;
+		double undouble = 19.99d;
+
+
+		//caracter
+		char uncaracter = 65;
+
+		//los datos primitivos son los que vienen en el mismo java, con excepcion del String
+
+	}
+
+}
+
