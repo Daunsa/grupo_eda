7d3997ca194ed6932d45cf2c93a5a3e377bc4444
Daniel Tapia <dtapias@unsa.edu.pe>
2022-05-05 20:15:51 -0500

    Se corrigio la identacion

--- Ejemplo_bucle_foreach.java
+++ Ejemplo_bucle_foreach.java
@@ -1,9 +1,9 @@
 public class Ejemplo_bucle_foreach {
     public static void main(String[] args) {
         System.out.println("Ejemplo de uso de bucle foreach:");
-        String[] animales = {"perro", "gato", "canario"};
-            for (String i : animales) {
-                System.out.println(i);
-            }
+        String[] animales = { "perro", "gato", "canario" };
+        for (String i : animales) {
+            System.out.println(i);
+        }
     }
 }
