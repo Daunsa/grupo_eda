3a75937dc73f26d221d3b375b4a905f74607a47d
RonaldGarciaValdivia <rgarciava@unsa.edu.pe>
2022-05-06 01:04:18 -0500

    Se creo ejemplo switch

---
+++ ejemplo_Switch.java
@@ -0,0 +1,22 @@
+public class ejemplo_Switch {
+	public static void main(String[]args) {
+		int boleto = 2;
+		switch (boleto) {
+		  case 1:
+		    System.out.println("Boleto perdedor");
+		    break;
+		  case 2:
+		    System.out.println("Boleto ganador");
+		    break;
+		  case 3:
+		    System.out.println("Boleto perdedor");
+		    break;
+		  case 4:
+		    System.out.println("Boleto perdedor");
+		    break;
+		  case 5:
+		    System.out.println("Boleto perdedor");
+		    break;
+		}
+	}
+}
